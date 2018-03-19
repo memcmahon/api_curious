fece2feb768ca47974acfd08ffc431823f725a2e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-19 13:56:14 -0600

    Adds sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
   def create
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    session[:id] = user.id
+    redirect_to root_path
   end
 end
