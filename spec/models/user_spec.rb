766ee26e308a48d8e7bc198d14c5454fa10c32be
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-19 14:17:43 -0600

    Adds model spec for user#from_omniauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "finds or creates itself from an oauth hash" do
+    auth = {
+      provider: "github",
+      uid: "123456",
+      info: {
+        nickname: "memcmahon",
+        name: "Megan McMahon",
+        email: "meganmcmahon88@gmail.com"
+      },
+      credentials: {
+        token: "1234abcd"
+      }
+    }
+
+    User.from_omniauth(auth)
+    user = User.first
+
+    expect(user.uid).to eq("123456")
+    expect(user.email).to eq("meganmcmahon88@gmail.com")
+    expect(user.nickname).to eq("memcmahon")
+    expect(user.name).to eq("Megan McMahon")
+    expect(user.token).to eq("1234abcd")
+  end
 end
