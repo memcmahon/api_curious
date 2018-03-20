7c40609207f6a1605789ff5b351e40888148c34e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-20 15:43:25 -0600

    Implements api calls for basic user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
-    where(uid: auth_info[:uid]).first_or_create do |user|
+    where(uid: auth_info[:uid]).first_or_create.update do |user|
       user.uid = auth_info[:uid]
       user.nickname = auth_info[:info][:nickname]
       user.name = auth_info[:info][:name]
