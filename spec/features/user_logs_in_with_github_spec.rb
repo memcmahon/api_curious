87420c5f4b0e88cf3cc7529b4ca624af08e8b525
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-21 15:49:37 -0600

    Adds end to end feature test with webmock

--- spec/features/user_logs_in_with_github_spec.rb
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 describe "user logs in and out" do
   scenario "using github oath" do
+    user_json_response = File.open("./spec/fixtures/user.json")
+    user_stars_json_response = File.open("./spec/fixtures/user_stars.json")
+
+    stub_request(:get, "https://api.github.com/user?access_token=12345").
+      to_return(status: 200, body: user_json_response, headers: {})
+
+    stub_request(:get, "https://api.github.com/user/starred?access_token=12345").
+      to_return(status: 200, body: user_stars_json_response, headers: {})
+
     stub_omniauth
+
     visit root_path
+
     click_link("Sign in with Github")
     expect(page).to have_link("Logout")
 
@@ -13,7 +24,7 @@ describe "user logs in and out" do
     expect(page).to have_content("Backend Engineering Student at Turing School")
     expect(page).to have_content("Denver, CO")
     expect(page).to have_content("Repositories 39")
-    expect(page).to have_content("Stars 0")
+    expect(page).to have_content("Stars 1")
     expect(page).to have_content("Followers 1")
     expect(page).to have_content("Following 0")
 
@@ -32,7 +43,7 @@ describe "user logs in and out" do
         nickname: "memcmahon"
       },
       credentials: {
-        token: ENV["ACCESS_TOKEN"],
+        token: 12345,
       }
     })
   end
