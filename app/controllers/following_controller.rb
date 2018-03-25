f4d93c28e275aa9061ae893fbaf6b863e534a994
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-25 16:46:12 -0600

    Refactors github service

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -7,25 +7,3 @@ class FollowingController < ApplicationController
     @events = Events.new(params[:username]).all
   end
 end
-
-class Events
-  def initialize(username)
-    @username = username
-  end
-
-  def pages
-    binding.pry
-    GithubService.event_pages(@username)[:link].split("page=")[2].to_i
-  end
-
-
-  def all
-    events = []
-    pages.times do |n|
-      events << GithubService.events(@username, n+1)
-    end
-    events.flatten.map do |event|
-      Event.new(event)
-    end
-  end
-end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,47 +1,48 @@
-require 'date'
-
 class GithubService
+  def self.conn(url)
+    Faraday.new("https://api.github.com#{url}")
+  end
+
+  def self.parse(response_body)
+    JSON.parse(response_body, symbolize_names: true)
+  end
+
   def self.user_attrs(token)
-    conn = Faraday.new("https://api.github.com/user")
-    response = conn.get do |req|
+    response = conn("/user").get do |req|
       req.params["access_token"] = token
     end
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response.body)
   end
 
   def self.stars_count(token)
-    conn = Faraday.new("https://api.github.com/user/starred")
-    response = conn.get do |req|
+    response = conn("/user/starred").get do |req|
       req.params["access_token"] = token
     end
-    JSON.parse(response.body, symbolize_names: true).count
+    parse(response.body).count
   end
 
   def self.user_commits(token, username)
-    conn = Faraday.new("https://api.github.com/search/commits")
-    response = conn.get do |req|
+    response = conn("/search/commits").get do |req|
       req.headers["Accept"] = "application/vnd.github.cloak-preview+json"
       req.params["access_token"] = token
       req.params["q"] = "author:#{username} committer-date:>#{DateTime.now.prev_day(7).strftime("%Y-%m-%d")}"
     end
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response.body)
   end
 
   def self.following(token)
-    conn = Faraday.new("https://api.github.com/user/following")
-    response = conn.get do |req|
+    response = conn("/user/following").get do |req|
       req.params["access_token"] = token
     end
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response.body)
   end
 
   def self.event_pages(username)
-    response = Faraday.get("http://api.github.com/users/#{username}/events")
-    response.headers
+    Faraday.get("http://api.github.com/users/#{username}/events").headers
   end
 
   def self.events(username, n)
-    response = Faraday.get("http://api.github.com/users/#{username}/events?page=#{n}")
-    JSON.parse(response.body, symbolize_names: true)
+    response = conn("/#{username}/events?page=#{n}").get
+    parse(response.body)
   end
 end
